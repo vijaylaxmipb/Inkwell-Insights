3c41b85cd101e3f5ba4aa25cb391df2eb49831e2
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-08-24 00:28:09 +0000

    add secret key to env file

--- inkwellinsights/settings.py
+++ inkwellinsights/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-20mm5o)&%6776$g+va31!10is942t35^99f^(&9cn(r-bkqk&b'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-vijaylaxmip-inkwellinsi-nxx7sagboaw.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [
+    '8000-vijaylaxmip-inkwellinsi-nxx7sagboaw.ws.codeinstitute-ide.net',
+    ".herokuapp.com"]
 
 
 # Application definition
@@ -74,11 +80,15 @@ WSGI_APPLICATION = 'inkwellinsights.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
