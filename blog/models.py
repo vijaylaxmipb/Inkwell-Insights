3ce48bf8d7e0516e66e0a7fbf506e7c6f092973b
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-08-27 22:45:38 +0000

    Fixed auth_user table error by applying migrations

--- blog/models.py
+++ blog/models.py
@@ -14,9 +14,22 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+       
 
 
 
