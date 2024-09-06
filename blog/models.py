d3c9aea1eaabe7732a60e35e9bdb195f0e53ab5b
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-09-06 07:55:55 +0000

    add methods to comment model

--- blog/models.py
+++ blog/models.py
@@ -18,21 +18,25 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-        
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter", default=1)  # Assuming '1' is the ID of the user to use as the default
     email = models.EmailField()
     body = models.TextField()
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=False)
-    challenge = models.SlugField()
+    active = models.BooleanField(default=False)    
+    
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
-  
+class Meta:
+        ordering = ["created_on"]
+
 
 
